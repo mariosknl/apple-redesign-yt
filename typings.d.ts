2470347ec28db7b0305cbeea73b1e2f648f42f97
mariosknl <mariosknl@outlook.com>
2022-12-03 12:10:42 +0200

    connecting to sanity & fetching categories

---
+++ typings.d.ts
@@ -0,0 +1,12 @@
+interface Category {
+  _id: string;
+  _createAt: string;
+  _updatedAt: string;
+  _rev: string;
+  _type: "category";
+  slug: {
+    _type: "slug";
+    current: string;
+  };
+  title: string;
+}
