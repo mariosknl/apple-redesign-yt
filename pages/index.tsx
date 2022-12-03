21963bee377aad7492a4d371af50a940c557e52e
mariosknl <mariosknl@outlook.com>
2022-12-03 11:03:18 +0200

    Button component created

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,23 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
+import Landing from "../components/Landing";
 
 const Home: NextPage = () => {
-	return (
-		<div className="">
-			<Head>
-				<title>Apple Redesign</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+  return (
+    <div className="">
+      <Head>
+        <title>Apple Redesign</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-			<Header />
-		</div>
-	);
+      <Header />
+
+      <main className="relative h-[200vh] bg-[#E7ECEE]">
+        <Landing />
+      </main>
+    </div>
+  );
 };
 
 export default Home;
