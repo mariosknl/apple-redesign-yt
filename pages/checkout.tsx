6bf164b4b0e44e68bdc0abb989c8ea212679714a
mariosknl <mariosknl@outlook.com>
2022-12-03 21:03:35 +0200

    creating checkout page

--- pages/checkout.tsx
+++ pages/checkout.tsx
@@ -1,17 +1,19 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import { selectBasketItems } from "../redux/basketSlice";
+import { selectBasketItems, selectBasketTotal } from "../redux/basketSlice";
 import { useSelector } from "react-redux";
 import { ChevronDownIcon } from "@heroicons/react/24/solid";
 import Button from "../components/Button";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from "react-currency-formatter";
 
 type Props = {};
 
 const checkout = (props: Props) => {
   const items = useSelector(selectBasketItems);
+  const basketTotal = useSelector(selectBasketTotal);
   const router = useRouter();
   const [groupedItemsInBasket, setGroupedItemsInBasket] = useState(
     {} as { [key: string]: Product[] },
@@ -51,6 +53,77 @@ const checkout = (props: Props) => {
             {Object.entries(groupedItemsInBasket).map(([key, items]) => (
               <CheckoutProduct key={key} items={items} id={key} />
             ))}
+
+            <div className="my-12 mt-6 ml-auto max-w-3xl">
+              <div className="divide-y divide-gray-300">
+                <div className="pb-4">
+                  <div className="flex justify-between">
+                    <p>Subtotal</p>
+                    <p>
+                      <Currency quantity={basketTotal} currency="EUR" />
+                    </p>
+                  </div>
+                  <div className="flex justify-between">
+                    <p>Shipping</p>
+                    <p>FREE</p>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex flex-col gap-x-1 lg:flex-row">
+                      Estimated tax for:
+                      <p className="flex cursor-pointer items-end text-blue-500 hover:underline">
+                        Enter zip code
+                        <ChevronDownIcon className="h-6 w-6" />
+                      </p>
+                    </div>
+                    <p>$ -</p>
+                  </div>
+                </div>
+
+                <div className="flex justify-between pt-4 text-xl font-semibold">
+                  <h4>Total</h4>
+                  <h4>
+                    <Currency quantity={basketTotal} currency="EUR" />
+                  </h4>
+                </div>
+              </div>
+
+              <div className="my-14 space-y-4">
+                <h4 className="text-xl font-semibold">How would you like to check out?</h4>
+                <div className="flex flex-col gap-4 md:flex-row">
+                  <div className="order-2 flex flex-1 flex-col items-center rounded-xl bg-gray-200 p-8 py-12 text-center">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      <span>Pay Monthly</span>
+                      <span>with Apple Card</span>
+                      <span>
+                        $283.16/mo. at 0% APR<sup className="-top-1">◊</sup>
+                      </span>
+                    </h4>
+                    <Button title="Check Out with Apple Card Monthly Installements" />
+                    <p className="mt-2 max-w-[240px] text-[13px]">
+                      $0.00 due today, which includes applicable full-price items, down payments,
+                      shipping, and taxes
+                    </p>
+                  </div>
+
+                  <div className="flex flex-1 flex-col items-center space-y-8 rounded-xl bg-gray-200 p-8 py-12 md:order-2">
+                    <h4 className="mb-4 flex flex-col text-xl font-semibold">
+                      Pay in full
+                      <span>
+                        <Currency quantity={basketTotal} currency="USD" />
+                      </span>
+                    </h4>
+
+                    <Button
+                      noIcon
+                      // loading={loading}
+                      title="Check Out"
+                      width="w-full"
+                      // onClick={createCheckoutSession}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         )}
       </main>
