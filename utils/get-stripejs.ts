df0645921e8633da915b26122e1d736497635cf9
mariosknl <mariosknl@outlook.com>
2022-12-04 11:10:50 +0200

    updating get0stripejs.ts file

--- utils/get-stripejs.ts
+++ utils/get-stripejs.ts
@@ -5,7 +5,7 @@ import { loadStripe, Stripe } from "@stripe/stripe-js";
 let stripePromise: Promise<Stripe | null>;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
+    stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!}`);
   }
   return stripePromise;
 };
